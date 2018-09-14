85cc923bc4db67b4847d161646d26fb0a7bc2f59
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-14 11:58:14 -0700

    search fix

--- server.js
+++ server.js
@@ -79,7 +79,7 @@ app.post('/api/auth/signin', (req, res) => {
     });
 });
 
-app.use('/api', (req, res, next) => {
+app.use((req, res, next) => {
   const id = req.get('Authorization');
   if(!id) {
     res.status(403).send({
@@ -237,16 +237,16 @@ app.get('/api/airdate', (req, res, next) => {
 
 app.get('/api/search/:keywords', (req, res, next) => {
   let keywords = req.params.keywords;
-  console.log('req', req.params);
+  console.log('server keywords!!!!!!!!!!', keywords);
   client.query(`
-  SELECT c.id, c.round, category, c.value, c.clue, c.answer, airdate
+  SELECT c.id, c.round, category, c.value, c.clue, c.answer
   FROM historic_clues as c
-  JOIN historic_airdates ON c.game_id = historic_airdates.id
   JOIN historic_categories ON c.category_id = historic_categories.id
   WHERE category LIKE '%${keywords}%' OR c.clue LIKE '%${keywords}%' OR c.answer LIKE '%${keywords}%'
   ORDER BY(round, category, value);
   `)
     .then(result => {
+      console.log('RESULT!!!!!', result);
       res.send(result.rows);
     })
     .catch(next);
