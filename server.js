b75ae33de02c375a2a00a290cb1bf81fd7fd833d
Sarah Flynn <sarah.flynn@icloud.com>
2018-09-14 10:03:36 -0700

    server fix

--- server.js
+++ server.js
@@ -92,18 +92,15 @@ app.use('/api', (req, res, next) => {
   next();
 });
 
-// app.use((req, res) => {
-//   res.sendFile('index.html', { root: 'public' }) ;
-// });
 
 app.post('/api/games/:className/:boardId', (req, res, next) => {
   let className = req.params.className;
   let boardId = req.params.boardId;
   if(className === 'error' || boardId === 'error') return next('bad name');
   client.query(`
-    INSERT INTO games(class_name, board_id)
-    VALUES ($1, $2)
-    RETURNING *, board_id as "boardId";
+  INSERT INTO games(class_name, board_id)
+  VALUES ($1, $2)
+  RETURNING *, board_id as "boardId";
   `,
   [className, boardId]
   ).then(result => {
@@ -116,10 +113,10 @@ app.post('/api/teams/:teamName', (req, res, next) => {
   let teamName = req.params.teamName;
   if(teamName === 'error') return next('bad teamName');
   client.query(`
-    INSERT INTO teams(team, score)
-    VALUES ($1, 0)
-    RETURNING *, id as "teamId";
-    `,
+  INSERT INTO teams(team, score)
+  VALUES ($1, 0)
+  RETURNING *, id as "teamId";
+  `,
   [teamName]
   ).then(result => {
     res.send(result.rows[0]);
@@ -133,10 +130,10 @@ app.post('/api/teams/games/:teamId/:gameId', (req, res, next) => {
   let gameId = req.params.gameId;
   if(teamId === 'error' || gameId === 'error') return next('bad teamId or gameId when adding to team_game');
   client.query(`
-    INSERT INTO team_game (team_id, game_id)
-    VALUES ($1, $2)
-    RETURNING *;
-    `,
+  INSERT INTO team_game (team_id, game_id)
+  VALUES ($1, $2)
+  RETURNING *;
+  `,
   [teamId, gameId]
   ).then(result => {
     res.send(result.rows[0]);
@@ -146,10 +143,10 @@ app.post('/api/teams/games/:teamId/:gameId', (req, res, next) => {
 
 app.get('/api/me/boards', (req, res, next) => {
   client.query(`
-    SELECT id, name
-    FROM boards
-    WHERE user_id = $1
-    ORDER BY id; 
+  SELECT id, name
+  FROM boards
+  WHERE user_id = $1
+  ORDER BY id; 
   `,
   [req.userId])
     .then(result => {
@@ -162,10 +159,10 @@ app.get('/api/teams/:gameId', (req, res, next) => {
   let gameId = req.params.gameId;
   if(gameId === 'error') return next('bad gameId');
   client.query(`
-    SELECT teams.team, teams.id
-    FROM teams
-    JOIN team_game ON team_game.team_id = teams.id
-    WHERE team_game.game_id = $1
+  SELECT teams.team, teams.id
+  FROM teams
+  JOIN team_game ON team_game.team_id = teams.id
+  WHERE team_game.game_id = $1
   `,
   [gameId]
   ).then(result => {
@@ -175,11 +172,11 @@ app.get('/api/teams/:gameId', (req, res, next) => {
 });
 app.get('/api/games-played', (req, res) => {
   client.query(`
-    SELECT DISTINCT games.class_name, games.id
-    FROM games
-    JOIN boards ON boards.id = games.board_id
-    WHERE boards.user_id = $1
-    ORDER BY games.id
+  SELECT DISTINCT games.class_name, games.id
+  FROM games
+  JOIN boards ON boards.id = games.board_id
+  WHERE boards.user_id = $1
+  ORDER BY games.id
   `,
   [req.userId]
   )
@@ -197,7 +194,7 @@ app.get('/api/scores/:gameId', (req, res, next) => {
   JOIN team_game ON team_game.team_id = teams.id
   JOIN games ON games.id = team_game.game_id
   WHERE games.id = $1;
-    `,
+  `,
   [gameId]
   ).then(result => {
     res.send(result.rows);
@@ -209,12 +206,12 @@ app.get('/api/results/:id', (req, res, next) => {
   let gameId = req.params.gameId;
   if(gameId === 'error') return next('bad gameId');
   client.query(`
-    SELECT *
-    FROM teams
-    JOIN team_game on teams.id = team_game.team_id
-    JOIN games on games.id = $1
-    RETURNING *;
-    `,
+  SELECT *
+  FROM teams
+  JOIN team_game on teams.id = team_game.team_id
+  JOIN games on games.id = $1
+  RETURNING *;
+  `,
   [gameId]
   ).then(result => {
     res.send(result.rows[0]);
@@ -224,12 +221,12 @@ app.get('/api/results/:id', (req, res, next) => {
 
 app.get('/api/airdate', (req, res, next) => {
   client.query(`
-    SELECT c.id, c.round, category, c.value, c.clue, c.answer, airdate
-    FROM historic_clues as c
-    JOIN historic_airdates ON c.game_id = historic_airdates.id
-    JOIN historic_categories ON c.category_id = historic_categories.id
-    WHERE airdate = '2001-07-03'
-    ORDER BY(round, category, value);
+  SELECT c.id, c.round, category, c.value, c.clue, c.answer, airdate
+  FROM historic_clues as c
+  JOIN historic_airdates ON c.game_id = historic_airdates.id
+  JOIN historic_categories ON c.category_id = historic_categories.id
+  WHERE airdate = '2001-07-03'
+  ORDER BY(round, category, value);
   `)
     .then(result => {
       res.send(result.rows);
@@ -258,12 +255,12 @@ app.get('/api/search/:keywords', (req, res, next) => {
 app.get('/api/categories/:id', (req, res, next) => {
   let gameId = req.params.id;
   client.query(`
-    SELECT DISTINCT categories.category, clues.category_id
-    FROM games as g
-    JOIN boards ON g.board_id = boards.id
-    JOIN categories ON boards.id = categories.board_id
-    JOIN clues ON categories.id = category_id
-    WHERE g.id = $1;
+  SELECT DISTINCT categories.category, clues.category_id
+  FROM games as g
+  JOIN boards ON g.board_id = boards.id
+  JOIN categories ON boards.id = categories.board_id
+  JOIN clues ON categories.id = category_id
+  WHERE g.id = $1;
   `,
   [gameId]
   ).then(result => {
@@ -275,12 +272,12 @@ app.get('/api/categories/:id', (req, res, next) => {
 app.get('/api/game/:id', (req, res, next) => {
   let gameId = req.params.id;
   client.query(`
-    SELECT g.class_name, g.board_id, boards.name, categories.category, clues.category_id, clues.clue, clues.id, clues.answer, clues.value
-    FROM games as g
-    JOIN boards ON g.board_id = boards.id
-    JOIN categories ON boards.id = categories.board_id
-    JOIN clues ON categories.id = category_id
-    WHERE g.id = $1;
+  SELECT g.class_name, g.board_id, boards.name, categories.category, clues.category_id, clues.clue, clues.id, clues.answer, clues.value
+  FROM games as g
+  JOIN boards ON g.board_id = boards.id
+  JOIN categories ON boards.id = categories.board_id
+  JOIN clues ON categories.id = category_id
+  WHERE g.id = $1;
   `,
   [gameId]
   ).then(result => {
@@ -293,12 +290,12 @@ app.post('/api/me/boards/:board', (req, res, next) => {
   let board = req.params.board;
   console.log('server board board', board);
   if(board === 'error') return next('bad name');
-
+  
   client.query(`
-      INSERT INTO boards (name, user_id)
-      VALUES ($1, $2)
-      RETURNING *, user_id as "userId";
-    `,
+  INSERT INTO boards (name, user_id)
+  VALUES ($1, $2)
+  RETURNING *, user_id as "userId";
+  `,
   [board, req.userId]
   ).then(result => {
     console.log('result', result.rows[0]);
@@ -311,12 +308,12 @@ app.post('/api/me/boards/:board/categories/:category', (req, res, next) => {
   let board = req.params.board;
   let category = req.params.category;
   if(board === 'error' || category === 'error') return next('bad input');
-
+  
   client.query(`
-      INSERT INTO categories (category, board_id)
-      VALUES ($1, $2)
-      RETURNING *, board_id as "boardId";
-    `,
+  INSERT INTO categories (category, board_id)
+  VALUES ($1, $2)
+  RETURNING *, board_id as "boardId";
+  `,
   [category, board]
   ).then(result => {
     console.log('result', result.rows[0]);
@@ -330,14 +327,14 @@ app.post('/api/me/categories/:category/clues/:clue/:answer/:value', (req, res, n
   let answer = req.params.answer;
   let value = req.params.value;
   let category = req.params.category;
-
+  
   if(clue === 'error' || category === 'error') return next('bad input');
-
+  
   client.query(`
-      INSERT INTO clues (clue, answer, value, category_id)
-      VALUES ($1, $2, $3, $4)
-      RETURNING *, category_id as "categoryId";
-    `,
+  INSERT INTO clues (clue, answer, value, category_id)
+  VALUES ($1, $2, $3, $4)
+  RETURNING *, category_id as "categoryId";
+  `,
   [clue, answer, value, category]
   ).then(result => {
     console.log('result', result.rows[0]);
@@ -349,10 +346,10 @@ app.post('/api/me/categories/:category/clues/:clue/:answer/:value', (req, res, n
 app.get('/api/me/boards/categories/:id', (req, res, next) => {
   let boardId = req.params.id;
   client.query(`
-    SELECT DISTINCT categories.category
-    FROM boards as b
-    JOIN categories ON b.id = categories.board_id
-    WHERE b.id = $1;
+  SELECT DISTINCT categories.category
+  FROM boards as b
+  JOIN categories ON b.id = categories.board_id
+  WHERE b.id = $1;
   `,
   [boardId]
   ).then(result => {
@@ -364,11 +361,11 @@ app.get('/api/me/boards/categories/:id', (req, res, next) => {
 app.get('/api/me/boards/clues/:id', (req, res, next) => {
   let boardId = req.params.id;
   client.query(`
-    SELECT boards.name, categories.category, clues.category_id, clues.clue, clues.id, clues.answer, clues.value
-    FROM boards
-    JOIN categories ON boards.id = categories.board_id
-    JOIN clues ON categories.id = category_id
-    WHERE boards.id = $1;
+  SELECT boards.name, categories.category, clues.category_id, clues.clue, clues.id, clues.answer, clues.value
+  FROM boards
+  JOIN categories ON boards.id = categories.board_id
+  JOIN clues ON categories.id = category_id
+  WHERE boards.id = $1;
   `,
   [boardId]
   ).then(result => {
@@ -381,10 +378,10 @@ app.get('/api/me/boards/categoryNumber/:id', (req, res, next) => {
   let boardId = req.params.id;
   console.log('server', boardId);
   client.query(`
-    SELECT COUNT(categories.category)
-    FROM boards as b
-    JOIN categories ON b.id = categories.board_id
-    WHERE b.id = $1;
+  SELECT COUNT(categories.category)
+  FROM boards as b
+  JOIN categories ON b.id = categories.board_id
+  WHERE b.id = $1;
   `,
   [boardId]
   ).then(result => {
@@ -397,7 +394,7 @@ app.get('/api/me/boards/categoryNumber/:id', (req, res, next) => {
 app.delete('/api/deleteGames/:gameId', (req, res, next) => {
   let gameId = req.params.id;
   client.query(`
-    delete from games where id=$1;
+  delete from games where id=$1;
   `,
   [gameId]
   ).then(() => {
@@ -412,11 +409,11 @@ app.put('/api/game/:gameId/turn/:turn', (req, res, next) => {
   let turn = req.params.turn;
   if(gameId === 'error' || turn === 'error') return next('bad input');
   client.query(`
-    UPDATE games
-    SET turn = $2
-    WHERE games.id = $1
-    RETURNING games.turn;
-    `,
+  UPDATE games
+  SET turn = $2
+  WHERE games.id = $1
+  RETURNING games.turn;
+  `,
   [gameId, turn])
     .then(result => {
       res.send(result.rows);
@@ -427,52 +424,25 @@ app.put('/api/game/:gameId/turn/:turn', (req, res, next) => {
 app.get('/api/get-turn/:id', (req, res, next) => {
   let gameId = req.params.id;
   client.query(`
-    SELECT turn
-    FROM games
-    JOIN teams ON teams.team = games.turn
-    WHERE games.id = $1;
+  SELECT turn
+  FROM games
+  JOIN teams ON teams.team = games.turn
+  WHERE games.id = $1;
   `,
   [gameId]
   ).then(result => {
     res.send(result.rows);
   })
     .catch(next);
+}),
+        
+        
+        
+        
+        
+app.use((req, res) => {
+  res.sendFile('index.html', { root: 'public' }) ;
 });
-
-// app.put('/api/:gameId/add-turn/:turn', (req, res, next) => {
-//   let gameId = req.params.gameId;
-  
-//   let turn = req.params.turn;
-//   if(gameId === 'error' || turn === 'error') return next('bad input');
-//   client.query(`
-//     UPDATE games
-//     SET turn = $2
-//     WHERE games.id = $1;
-//     `,
-//   [gameId, turn])
-//     .catch(next)
-// });
-
-// SOFIE AND CLAIRE TRYING TO REWORK THINGS:
-// app.get('/api/get-turn/:id', (req, res, next) => {
-//   client.query(`
-//     SELECT turn, team
-//     FROM teams
-//   `,
-//   ).then(result => {
-//     res.send(result.rows);
-//   })
-//     .catch(next);
-// });
-
-// app.use((req, res) => {
-//   res.sendFile('index.html', { root: 'public' }) ;
-// });
-
-
-
-
-
-
+        
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log('server humming along on port', PORT));
